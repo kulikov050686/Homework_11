414331aaf0c0bd8dec6e5819a36a09060cc40fed
Kulikov Pavel <kulikov050686@yandex.ru>
2020-12-18 23:55:32 +0300

    Реализованно запуск хоста при старте приложения

--- Homework_11/App.xaml.cs
+++ Homework_11/App.xaml.cs
@@ -1,8 +1,38 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Windows;
+using ViewModels;
 
 namespace Homework_11
-{    
+{
     public partial class App : Application
     {
-    }
+        private static IHost _Host;
+
+        public static IHost Host => _Host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build(); 
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+            base.OnStartup(e);
+
+            await host.StartAsync().ConfigureAwait(false);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            var host = Host;
+            await host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+            _Host = null;
+        }
+    }    
 }

9b18dc006695219fe6c6b3c9dbe7fceff8b1b348
kulikov050686 <kulikov050686@yandex.ru>
2021-04-16 13:16:26 +0300

    Добавлен контрол топ менеджмента

---
+++ Homework_11/UserControls/GeneralManagerUserControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using Models;
+using System.ComponentModel;
+
+namespace UserControls
+{   
+    public partial class GeneralManagerUserControl : UserControl
+    {
+        #region Должность управляющего
+
+        public static readonly DependencyProperty TitleUCProperty =
+            DependencyProperty.Register(nameof(TitleUC),
+                                        typeof(string),
+                                        typeof(GeneralManagerUserControl),
+                                        new PropertyMetadata(default(string)));
+        [Description("Должность управляющего")]
+        public string TitleUC
+        {
+            get => (string)GetValue(TitleUCProperty);
+            set => SetValue(TitleUCProperty, value);
+        }
+
+        #endregion
+
+        #region Главный управляющий
+
+        public static readonly DependencyProperty SupervisorUCProperty =
+            DependencyProperty.Register(nameof(SupervisorUC),
+                                        typeof(Supervisor),
+                                        typeof(GeneralManagerUserControl), 
+                                        new PropertyMetadata(default(Supervisor)));
+        [Description("Управляющий")]
+        public Supervisor SupervisorUC
+        {
+            get => (Supervisor)GetValue(SupervisorUCProperty);
+            set => SetValue(SupervisorUCProperty, value);
+        }
+
+        #endregion
+
+        public GeneralManagerUserControl() => InitializeComponent();        
+    }
+}
---
+++ Homework_11/ViewModels/TopManagementViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Controllers;
+using Models;
+
+namespace ViewModels
+{
+    /// <summary>
+    /// Модель-Представление том менеджмента
+    /// </summary>
+    public class TopManagementViewModel : BaseClassViewModelINPC
+    {
+        #region Закрытые поля
+
+        Ministry _ministry;
+
+        #endregion
+
+        #region Генеральный директор
+
+        private GeneralDirector _generalDirector;
+        public GeneralDirector GeneralDirector
+        {
+            get => _generalDirector;
+            set => Set(ref _generalDirector, value);
+        }
+
+        #endregion
+
+        #region Главный бухгалтер
+
+        private ChiefAccountant _chiefAccountant;
+        public ChiefAccountant ChiefAccountant
+        {
+            get => _chiefAccountant;
+            set => Set(ref _chiefAccountant, value);
+        }
+
+        #endregion
+
+        #region Заместитель генерального директора
+
+        private DeputyDirector _deputyDirector;
+        public DeputyDirector DeputyDirector
+        {
+            get => _deputyDirector;
+            set => Set(ref _deputyDirector, value);
+        }
+
+        #endregion
+
+        #region Конструктор
+
+        public TopManagementViewModel(Ministry ministry)
+        {
+            _ministry = ministry;
+            _generalDirector = ministry.GeneralDirector;
+            _deputyDirector = ministry.DeputyDirector;
+            _chiefAccountant = ministry.ChiefAccountant;
+        }
+
+        #endregion
+    }
+}
